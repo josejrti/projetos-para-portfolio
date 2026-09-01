40bbb1f155459c65ac5888fd6e24a93f79d8967a
José Júnior <josejrcode@gmail.com>
2026-08-31 22:35:09 -0300

    Adiciona o projeto de calculadora simples

---
+++ calculadora-simples/script.js
@@ -0,0 +1,21 @@
+function AdicionarCaractere(value) {
+    document.getElementById('calc-display').value += value
+}
+
+function ApagarDisplay() {
+    document.getElementById('calc-display').value = ''
+}
+
+function ApagarCaractere () {
+    const display = document.getElementById('calc-display')
+    display.value = display.value.slice(0, -1)
+}
+
+function Calcular() {
+    const display = document.getElementById('calc-display')
+    try {
+        display.value = eval(display.value)
+    } catch (error) {
+        display.value = 'Erro'
+    }
+}
